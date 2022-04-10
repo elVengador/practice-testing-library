4dca3142239972bfcbfed762d84e0c072d754849
elVengador <jimynicanor1001@gmail.com>
2022-04-09 20:17:38 -0500

    test from counter

--- src/components/Counter.test.tsx
+++ src/components/Counter.test.tsx
@@ -1,4 +1,4 @@
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import { Counter } from './Counter'
 
 describe('should test Counter component', () => {
@@ -7,5 +7,18 @@ describe('should test Counter component', () => {
         render(<Counter />)
         expect(screen.queryByText('count is: 0')).toBeInTheDocument()
     });
+
+    test('should work controls from Counter', () => {
+        render(<Counter />)
+        expect(screen.queryByText('count is: 0')).toBeInTheDocument()
+        fireEvent.click(screen.getByText('Increment'))
+        fireEvent.click(screen.getByText('Increment'))
+        expect(screen.queryByText('count is: 2')).toBeInTheDocument()
+        fireEvent.click(screen.getByText('Decrement'))
+        expect(screen.queryByText('count is: 1')).toBeInTheDocument()
+        fireEvent.click(screen.getByText('Reset'))
+        expect(screen.queryByText('count is: 0')).toBeInTheDocument()
+
+    })
 
 });
